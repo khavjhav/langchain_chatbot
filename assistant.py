16bd9f8085538f679692c6423cad528fb7ac4763
aukik <aukikaurnabx@gmail.com>
2024-03-03 01:01:46 +0600

    GEMINI ADDED

--- assistant.py
+++ assistant.py
@@ -7,7 +7,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain.text_splitter import RecursiveJsonSplitter
 from langchain.docstore.document import Document
 from langchain_community.vectorstores import Qdrant
-
+from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
 from qdrant_client import QdrantClient, models
 from langchain_core.documents import Document
 import random
@@ -17,13 +17,20 @@ from pathlib import Path
 from pprint import pprint
 from threading import Thread
 import threading
+import getpass
+import os
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Provide your Google API Key")
 
 # model = Ollama(model="dolphin-mixtral")
 # model= Ollama(model="tinydolphin")
-model=CustomLLM()
+model = ChatGoogleGenerativeAI(model="gemini-1.0-pro-latest")
+# model=CustomLLM()
 ollama_emb = OllamaEmbeddings(
     model="nomic-embed-text",
 )
+# ollama_emb = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
 
 def createChain():
@@ -42,6 +49,8 @@ def createChain():
     You are a customer support helpful assistant for an agency that helps student to pursue their academics in abroad based on the context given.
     Your context will contain a list of documents each refering to a relevant postgraduate program retrieved from a vector database based on similarity search.
     You have to answer the following question like an helpful assistant. Do not mention about how you are getting the context. Behave like a human and you already know the context.
+    This context is provided by the system instead of customer so do not talk to customer about how you are getting information from the context.
+    Rather use the context to answer their query.
     [/INST]
     [INST]
     Question: {question}
@@ -79,12 +88,13 @@ def generator(question):
 
 def process_item(item, index):
     docs = Document(page_content=str(item))
-    db = Qdrant.from_documents([docs], ollama_emb, url="http://localhost:6333", collection_name="my_documents")
+    db = Qdrant.from_documents([docs], ollama_emb, url="http://localhost:6333", collection_name="my_documents_google")
     # Corrected way to get the current thread's name
     print(f"Processed item {index} in thread {threading.current_thread().name}")
 
 def embedJson():
-    file_path = './csvjson (8).json'
+    print("Processing items in parallel...")
+    file_path = './oxford.json'
     json_data = json.loads(Path(file_path).read_text())
 
     threads = []
@@ -109,7 +119,7 @@ def getContext(query):
     # embedding_vector = ollama_emb.embed_query(query)
     # print(embedding_vector)
     # docs = db.max_marginal_relevance_search(query, k=4, fetch_k=1000)
-    docs=db.similarity_search_with_score(query, k=4)
+    docs=db.similarity_search_with_score(query, k=10)
 
 
     return str(docs)
